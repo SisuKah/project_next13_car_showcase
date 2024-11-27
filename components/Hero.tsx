f26529ac9c23d818a5c3664931818f434dbbca4e
SisuK <nowweebsite@gmail.com>
2024-11-27 17:39:27 +0200

    Made hero for mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,31 @@
 "use client";
 
 import Image from "next/image";
+import { useState, useEffect } from "react";
 import { CustomButton } from "@components";
 import AddCars from "./AddCarsButton"; // Adjust this import path as needed
 
 const Hero = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
   const handleScroll = () => {
     const nextSection = document.getElementById("discover");
     if (nextSection) {
       nextSection.scrollIntoView({ behavior: "smooth" });
     }
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="relative h-screen w-full">
@@ -27,15 +42,17 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-col items-center justify-center px-8">
         
         {/* Logo at the top center */}
-        <div className="absolute top-40">
-          <Image 
-            src="/AutoLogo.png" 
-            alt="Auto Logo" 
-            width={150} 
-            height={50} 
-            className="object-contain"
-          />
-        </div>
+        {!isSmallScreen && (
+          <div className="absolute top-40">
+            <Image 
+              src="/AutoLogo.png" 
+              alt="Auto Logo" 
+              width={150} 
+              height={50} 
+              className="object-contain"
+            />
+          </div>
+        )}
 
         {/* Heading with spacing and style */}
         <h1 className="text-white text-6xl md:text-8xl font-extrabold leading-tight text-center mt-[200px]"> {/* Added margin-top to move text down */}
